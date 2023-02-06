65ce5d61fcb25b9330c7eec8790d8d2a8ad540a5
Murat Ozcan <muratkerem@gmail.com>
2023-02-06 10:03:35 -0600

    UI login logout

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,7 +22,6 @@ import './commands'
 /* eslint-disable no-undef */
 
 Cypress.Commands.add('login', (email, password) => {
-  // cy.session({email, password}, () => {
   cy.visit('/')
 
   const dig = () =>
@@ -51,10 +50,69 @@ Cypress.Commands.add('login', (email, password) => {
       .shadow()
       .find('#password')
 
-  name().type(email, {force: true, delay: 0})
-  pw().should('not.have.attr', 'disabled')
-  pw().type(password, {force: true, delay: 0}).type('{enter}')
-  // })
+  const signInBtn = () =>
+    cy
+      .get('amplify-authenticator.hydrated')
+      .shadow()
+      .find('amplify-sign-in')
+      .shadow()
+      .find('amplify-form-section')
+      .shadow()
+      .find('amplify-button')
+      .shadow()
+      .find('.button')
 
-  // return cy.visit('/')
+  name().type(email, {force: true, delay: 0})
+  pw().type(password, {force: true, delay: 0, scrollBehavior: false})
+  signInBtn().click({force: true, scrollBehavior: false})
 })
+
+// Cypress.Commands.add('sessionLogin', (email, password) => {
+//   cy.session({email, password}, () => {
+//     cy.visit('/')
+
+//     const dig = () =>
+//       cy
+//         .get('amplify-authenticator.hydrated')
+//         .shadow()
+//         .find('amplify-sign-in')
+//         .shadow()
+//         .find('amplify-auth-fields')
+//         .shadow()
+
+//     const name = () =>
+//       dig()
+//         .find('amplify-username-field')
+//         .shadow()
+//         .find('amplify-form-field')
+//         .shadow()
+//         .find('#username')
+//         .should('be.enabled')
+
+//     const pw = () =>
+//       dig()
+//         .find('amplify-password-field')
+//         .shadow()
+//         .find('amplify-form-field')
+//         .shadow()
+//         .find('#password')
+
+//     const signInBtn = () =>
+//       cy
+//         .get('amplify-authenticator.hydrated')
+//         .shadow()
+//         .find('amplify-sign-in')
+//         .shadow()
+//         .find('amplify-form-section')
+//         .shadow()
+//         .find('amplify-button')
+//         .shadow()
+//         .find('.button')
+
+//     name().type(email, {force: true, delay: 0})
+//     pw().type(password, {force: true, delay: 0, scrollBehavior: false})
+//     signInBtn().click({force: true, scrollBehavior: false})
+//   })
+
+//   return cy.visit('/')
+// })
