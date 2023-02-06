53fa269da481a6d7ac94add4c079797be9613bb7
Murat Ozcan <muratkerem@gmail.com>
2023-02-06 10:10:42 -0600

    10 sec login timeout

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
   it('should login and logout', () => {
     cy.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
-    cy.contains('Hey')
+    cy.contains('Hey', {timeout: 10000})
 
     cy.get('amplify-authenticator.hydrated > div > .hydrated').click()
     cy.get('amplify-authenticator.hydrated').should('be.visible')
