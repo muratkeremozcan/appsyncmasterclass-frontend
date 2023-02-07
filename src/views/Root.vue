aa954f45e36608006be344c1543aa9b33e240a88
Murat Ozcan <muratkerem@gmail.com>
2023-02-07 06:11:04 -0600

    7 done

---
+++ src/views/Root.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Root</h1>
+    <amplify-authenticator usernameAlias="email">
+      <amplify-sign-out />
+    </amplify-authenticator>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+}
+</script>
