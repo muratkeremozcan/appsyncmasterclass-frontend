daec60e85f28db92f1c3331362c7ff8a4e5939bc
Murat Ozcan <muratkerem@gmail.com>
2023-02-08 07:01:26 -0600

    9 done

--- src/router/auth.guard.js
+++ src/router/auth.guard.js
@@ -4,7 +4,6 @@ export default async (to, from, next) => {
   const isProtected = to.matched.some(route => route.meta.protected)
   const loggedIn = await Auth.currentUserInfo()
   if (isProtected && !loggedIn) {
-    // if the user is not logged in, redirect to Root
     next('/')
     return
   }
