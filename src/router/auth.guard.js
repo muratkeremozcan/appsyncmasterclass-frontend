9325823966c49cef2c9566874240b36a98e1eb7e
Murat Ozcan <muratkerem@gmail.com>
2023-02-08 07:40:31 -0600

    10 done

--- src/router/auth.guard.js
+++ src/router/auth.guard.js
@@ -4,7 +4,7 @@ export default async (to, from, next) => {
   const isProtected = to.matched.some(route => route.meta.protected)
   const loggedIn = await Auth.currentUserInfo()
   if (isProtected && !loggedIn) {
-    next('/')
+    next('/#')
     return
   }
   next()
