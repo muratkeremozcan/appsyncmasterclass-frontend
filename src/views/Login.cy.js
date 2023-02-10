ef734473f921d52cc83d570ae858cbdb3380886b
Murat Ozcan <muratkerem@gmail.com>
2023-02-10 08:24:47 -0600

    13 done

--- src/views/Login.cy.js
+++ src/views/Login.cy.js
@@ -3,6 +3,8 @@ import LogIn from './LogIn.vue'
 describe('LogIn', () => {
   it('should', () => {
     cy.mount(LogIn)
-    cy.contains('h1', 'LogIn')
+    cy.get(':nth-child(3) > .w-full').type('a')
+    cy.get(':nth-child(4) > .w-full').type('b')
+    cy.get('.rounded-full').should('be.visible')
   })
 })
