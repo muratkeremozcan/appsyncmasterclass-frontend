4b7bb48c42c91c920eab989a2d57cb839116b5e1
Murat Ozcan <muratkerem@gmail.com>
2023-02-11 08:11:57 -0600

    project ide in base config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'r8t63u',
+
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+
   e2e: {
     setupNodeEvents() {
       // no-op
