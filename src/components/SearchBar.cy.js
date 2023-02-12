865fc0dad409cc2c610565762d95b995db01a79f
Murat Ozcan <muratkerem@gmail.com>
2023-02-12 05:25:18 -0600

    15 done

---
+++ src/components/SearchBar.cy.js
@@ -0,0 +1,8 @@
+import SearchBar from './SearchBar'
+
+describe('SearchBar', () => {
+  it('should render correctly', () => {
+    cy.mount(SearchBar)
+    cy.get('.pl-12').should('be.visible')
+  })
+})
--- src/views/Home.cy.js
+++ src/views/Home.cy.js
@@ -3,6 +3,8 @@ import Home from './Home.vue'
 describe('Home', () => {
   it('should', () => {
     cy.mount(Home)
+    cy.get('.fab').should('be.visible')
     cy.contains('h1', 'Home')
+    cy.contains('Welcome to Twitter!')
   })
 })
