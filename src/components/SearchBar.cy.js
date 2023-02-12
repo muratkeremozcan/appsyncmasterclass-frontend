cd241ad3cbb2676b3e478822a85b90277494fd65
Murat Ozcan <muratkerem@gmail.com>
2023-02-12 06:04:43 -0600

    reproduce Cy Cloud issue

--- src/components/SearchBar.cy.js
+++ src/components/SearchBar.cy.js
@@ -3,6 +3,6 @@ import SearchBar from './SearchBar'
 describe('SearchBar', () => {
   it('should render correctly', () => {
     cy.mount(SearchBar)
-    cy.get('.pl-12').should('be.visible')
+    cy.get('.pl-12').should('not.be.visible')
   })
 })
