f34998cba8b824cb904e0125224290c80ed6494b
Murat Ozcan <muratkerem@gmail.com>
2023-02-16 19:59:57 -0600

    compare cy-session to cy-data-session

--- cypress/e2e/data-session-login.cy.js
+++ cypress/e2e/data-session-login.cy.js
@@ -3,7 +3,7 @@ describe('sessionLogin', () => {
     cy.dataSessionLogin(Cypress.env('USERNAME'), Cypress.env('PASSWORD')),
   )
 
-  Cypress._.times(5, () => {
+  Cypress._.times(2, () => {
     it('should login', () => {
       cy.contains('Home')
     })
