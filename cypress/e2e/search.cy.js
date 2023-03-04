6aa1b3285c20e2bc15d5e40df75bdbfb205cb3d9
Murat Ozcan <muratkerem@gmail.com>
2023-03-04 06:00:59 -0600

    added search e2e

---
+++ cypress/e2e/search.cy.js
@@ -0,0 +1,15 @@
+describe('Profile', () => {
+  it('should upload profile picture', {defaultCommandTimeout: 10000}, () => {
+    cy.stubGqlRequest('getMyTimeline', {
+      fixture: 'getMyTimeline.json',
+    })
+    cy.stubGqlRequest('getMyProfile', {fixture: 'getMyProfile.json'})
+    cy.sessionLogin(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
+    cy.wait('@getMyTimeline')
+
+    cy.stubGqlRequest('search', {fixture: 'search.json'})
+    cy.get('.pl-12').type(']3ZgC&J8sqsgMtcY{enter}')
+    cy.wait('@search')
+    cy.contains('Replying to @appsynctesterQCNAWFW5')
+  })
+})
