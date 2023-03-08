4562e671328878b0245c9293f21f118abb9a3e56
Murat Ozcan <muratkerem@gmail.com>
2023-03-08 06:04:02 -0600

    0901 mobile styling

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full mt-5 flex justify-center items-center flex-col">
+  <div class="w-full mt-5 flex justify-center items-center flex-col p-5 md:p-0">
     <i class="fab fa-twitter text-blue text-4xl mb-8"></i>
     <p class="font-bold text-2xl mb-4">Log in to Twitter</p>
-    <div class="w-1/3 bg-lightblue border-b-2 border-dark mb-4 p-2">
+    <div class="w-full md:w-1/3 bg-lightblue border-b-2 border-dark mb-4 p-2">
       <p class="text-dark">Phone, email, or username</p>
       <input
         v-model="email"
@@ -11,7 +11,7 @@
         ref="email"
       />
     </div>
-    <div class="w-1/3 bg-lightblue border-b-2 border-dark mb-4 p-2">
+    <div class="w-full md:w-1/3 bg-lightblue border-b-2 border-dark mb-4 p-2">
       <p class="text-dark">Password</p>
       <input
         v-model="password"
@@ -21,7 +21,7 @@
     </div>
     <button
       @click.prevent="signIn"
-      class="w-1/3 font-bold rounded-full bg-blue text-white p-3 pl-3 pr-3 hover:bg-darkblue"
+      class="w-full md:w-1/3 font-bold rounded-full bg-blue text-white p-3 pl-3 pr-3 hover:bg-darkblue"
     >
       Log in
     </button>
@@ -30,7 +30,6 @@
 
 <script>
 import {mapActions} from 'vuex'
-
 export default {
   name: 'LogIn',
   data() {
